4db382c1436912534ed37bb8e0358f188fcd5e44
Greg Lewis <glewis@eyesbeyond.com>
2020-03-01 09:37:09 -0800

    Patch taken from FreeBSD ports repository:
    Fix printing services.  When CUPS is used, ${LOCALBASE}/bin/lpr must be
    used to print a generated PostScript file.  When lpd(8) is used, lpr(1) from
    base must be used.  Also, status command for lpc(8) requires a printer name.
    If no argument is specified, i.e., "/usr/sbin/lpc status", then it displays
    the command usage, i.e., "usage: status  {all | printer ...}".
    Unfortunately, "usage" is interpreted as a printer name because ":" is
    included.  Add "all" and adjust an expression for grep(1).
    
    Author: jkim@FreeBSD.org

--- jdk/src/solaris/classes/sun/print/UnixPrintServiceLookup.java
+++ jdk/src/solaris/classes/sun/print/UnixPrintServiceLookup.java
@@ -181,12 +181,12 @@ public class UnixPrintServiceLookup extends PrintServiceLookup
     static int cmdIndex = UNINITIALIZED;
 
     String[] lpcFirstCom = {
-        "/usr/sbin/lpc status | grep : | sed -ne '1,1 s/://p'",
+        "/usr/sbin/lpc status all | grep ':$' | sed -ne '1,1 s/://p'",
         "/usr/sbin/lpc status | grep -E '^[ 0-9a-zA-Z_-]*@' | awk -F'@' '{print $1}'"
     };
 
     String[] lpcAllCom = {
-        "/usr/sbin/lpc status all | grep : | sed -e 's/://'",
+        "/usr/sbin/lpc status all | grep ':$' | sed -e 's/://'",
         "/usr/sbin/lpc status all | grep -E '^[ 0-9a-zA-Z_-]*@' | awk -F'@' '{print $1}' | sort"
     };
 
